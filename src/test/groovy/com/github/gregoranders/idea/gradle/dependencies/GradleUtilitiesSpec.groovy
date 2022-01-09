52ff0b7cdf5b749053cfb7201321e64bfad23d98
Gregor Anders <gregor.anders@gmail.com>
2022-01-09 14:00:46 +0100

    [#14] Adds multi project with dependencies
    Signed-off-by: Gregor Anders <gregor.anders@gmail.com>

--- src/test/groovy/com/github/gregoranders/idea/gradle/dependencies/GradleUtilitiesSpec.groovy
+++ src/test/groovy/com/github/gregoranders/idea/gradle/dependencies/GradleUtilitiesSpec.groovy
@@ -129,6 +129,69 @@ class GradleUtilitiesSpec extends Specification {
             noExceptionThrown()
     }
 
+    def 'should return a multi project with dependencies'() {
+        given: 'a path to a multi project with dependencies'
+            def path = getProjectPath('multi-with-dependencies')
+        when: 'the test subject invokes getDependencies with this path'
+            def project = testSubject.getDependencies(path)
+        then: 'the name of the project should be "multi-with-dependencies"'
+            project.name() == 'multi-with-dependencies'
+        and: 'the version of the project should be "0.0.1"'
+            project.version() == '0.0.1'
+        and: 'the description of the project should be "Multi project with dependencies"'
+            project.description() == 'Multi project with dependencies'
+        and: 'the project should have no dependencies in all configurations'
+            project.configurations().forEach(configuration -> {
+                assert configuration.dependencies().size() == 0
+            })
+        and: 'the project should have 3 sub projects'
+            project.subProjects().size() == 3
+        and: 'the first sub project should be named "api"'
+            project.subProjects()[0].name() == 'api'
+        and: 'it should have the version "0.0.2"'
+            project.subProjects()[0].version() == '0.0.2'
+        and: 'it should have no dependencies in all configurations'
+            project.subProjects()[0].configurations().forEach(configuration -> {
+                assert configuration.dependencies().size() == 0
+            })
+        and: 'the second sub project should be named "impl"'
+            project.subProjects()[1].name() == 'impl'
+        and: 'it should have the version "0.0.2"'
+            project.subProjects()[1].version() == '0.0.3'
+        and: 'it should have 2 dependencies in the implementation configuration'
+            project.subProjects()[1].configurations().forEach(configuration -> {
+                if (configuration.name() == 'implementation') {
+                    assert configuration.dependencies().size() == 2
+
+                    assertDependency(configuration.dependencies()[0], 'com.github.gregoranders', 'api', '0.0.2')
+                    assertDependency(configuration.dependencies()[1], 'org.slf4j', 'slf4j-api', '1.7.32')
+                }
+            })
+        and: 'the third sub project should be named "test"'
+            project.subProjects()[2].name() == 'test'
+        and: 'it should have the version "0.0.4"'
+            project.subProjects()[2].version() == '0.0.4'
+        and: 'it should have 2 dependencies in the implementation configuration'
+            project.subProjects()[2].configurations().forEach(configuration -> {
+                if (configuration.name() == 'implementation') {
+                    assert configuration.dependencies().size() == 2
+
+                    assertDependency(configuration.dependencies()[0], 'com.github.gregoranders', 'impl', '0.0.3')
+                    assertDependency(configuration.dependencies()[1], 'org.slf4j', 'slf4j-api', '1.7.32')
+                }
+            })
+        and: 'it should have 1 dependencies in the testImplementation configuration'
+            project.subProjects()[2].configurations().forEach(configuration -> {
+                if (configuration.name() == 'testImplementation') {
+                    assert configuration.dependencies().size() == 1
+
+                    assertDependency(configuration.dependencies()[0], 'org.junit.jupiter', 'junit-jupiter-engine', '5.8.2')
+                }
+            })
+        and: 'no exceptions are thrown'
+            noExceptionThrown()
+    }
+
     def 'should throw an exception'() {
         given: 'an invalid path'
             def path = Path.of('foo')
